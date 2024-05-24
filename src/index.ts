c6f5b1789fb0daab3d44ca04fdb0696ddf313c77
AdityaSahu786 <sahuadi786183@gmail.com>
2024-05-25 03:57:07 +0530

    added a simple decentralised-exchange

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+let ETH_BALANCE = 200;
+let USDC_BALANCE = 700000;
+
+/* app.post("/add-liquidity", (req, res) => {
+
+}) */
+
+app.post("/buy-asset", (req, res) => {
+    const product = ETH_BALANCE * USDC_BALANCE
+    const quantity = req.body.quantity;
+    const updatedEthQuantity = ETH_BALANCE - quantity;
+    const updatedUsdcBalance = ETH_BALANCE * USDC_BALANCE/ updatedEthQuantity;
+    const paidAmount = updatedUsdcBalance - USDC_BALANCE;
+     
+
+    ETH_BALANCE = updatedEthQuantity;
+    USDC_BALANCE = updatedUsdcBalance;
+    res.json({
+        message: `You paid ${paidAmount} USDC for ${quantity} ETH`
+    })
+});
+
+app.post("/sell-asset", (req, res) => {
+    const quantity = req.body.quantity;
+    const updatedEthQuantity = ETH_BALANCE - quantity;
+    const updatedUsdcBalance = ETH_BALANCE * USDC_BALANCE / updatedEthQuantity;
+    const gottenUsdc = updatedUsdcBalance - USDC_BALANCE;
+
+    res.json({
+        message: `You got ${gottenUsdc} USDC for ${quantity} Eth`
+    })
+});
+
+app.listen(3000);
